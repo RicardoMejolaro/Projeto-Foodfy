8cd0f4e78c0e655296955da7e7ab4c9c9c2e478e
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-06-29 13:49:09 -0300

    configuring template engine nunjucks and starting the views

--- foodfy-dinamic-version/server.js
+++ foodfy-dinamic-version/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.set('view engine', 'njk');
 
+nunjucks.configure('views', {
+  express: server,
+  autoescape: false,
+  noCache: true
+});
 
 server.listen(5000, () => {
   console.log("Server is running!")
