c578c90de4d48f07003fee0192c735dee93a30a9
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-06-30 15:41:14 -0300

    included in the watch recipe details page the options to show and hide the details of ingredients, method of preparation and information with JS and CSS

--- foodfy-dinamic-version/public/scripts/scripts.js
+++ foodfy-dinamic-version/public/scripts/scripts.js
@@ -1,10 +1,46 @@
+//Lógica clica no card da página revenue e redireciona para uma nova página
+//com os detalhes da receita clicada
 const cards = document.querySelectorAll('.card');
 
 for (const card of cards) {
   card.addEventListener('click', () => {
     const recipeId = card.getAttribute('id');
     window.location.href = `receitas/${recipeId}`;
-  })
-  
+  });
 }
+
+//Lógica que mostra os esconde os detalhes de ingredientes, modo de preparo e
+//informações adicionais da página watch recipe details
+const linkView = document.querySelectorAll('div .head a');
+const divView = document.querySelectorAll('div .content-recipe-details');
+
+let linkId = 0;
+let divId = 0;
+for (const link of linkView) {
+  link.addEventListener('click', () => {
+    linkId = link.getAttribute('id');
+
+    for (const div of divView) {
+      divId = div.getAttribute('id')
+      const linkSelected = document.querySelector(`a#${divId}`);
+
+      if (linkId == divId) {
+        if (div.classList.contains(linkId)) {
+          div.classList.remove(linkId);
+          linkSelected.innerHTML = "Esconder";
+        } else {
+          div.classList.add(linkId);
+          linkSelected.innerHTML = 'Mostrar';
+        }
+      } 
+    }
+  });
+}
+
+
+
+
+
+
+
 
